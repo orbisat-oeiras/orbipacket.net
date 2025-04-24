b8e0a70374793f73c8c14fd40eed40ff70c7a35b
Marcelo Lago <72633651+mslago@users.noreply.github.com>
2025-04-24 18:35:45 +0100

    feat: update DeviceIds to match with the CanSat firmware.

--- Orbipacket.Library/DeviceId.cs
+++ Orbipacket.Library/DeviceId.cs
@@ -2,9 +2,12 @@ namespace Orbipacket
 {
     public enum DeviceId : byte
     {
-        Pressure = 0,
-        Temperature = 1,
-        Humidity = 2,
+        System = 1,
+        PressureSensor = 2,
+        TemperatureSensor = 3,
+        HumiditySensor = 4,
+        GPS = 5,
+        Accelerometer = 6,
         Unknown = 31,
     }
 }
