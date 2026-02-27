17eca15b7c888e279f2062396b3d7a5ee72f51cc
Marcelo Lago <72633651+mslago@users.noreply.github.com>
2026-02-27 13:35:43 +0000

    feat! (breaking change): change devideIDs to match orbipacket-rs

--- Orbipacket.Library/DeviceId.cs
+++ Orbipacket.Library/DeviceId.cs
@@ -2,12 +2,21 @@ namespace Orbipacket
 {
     public enum DeviceId : byte
     {
-        System = 1,
-        PressureSensor = 2,
-        TemperatureSensor = 3,
-        HumiditySensor = 4,
-        GPS = 5,
-        Accelerometer = 6,
-        Unknown = 31,
+        System = 0,
+        TimeSync = 1,
+        Gps = 2,
+        Camera = 3,
+        Accelerometer = 4,
+        Gyroscope = 5,
+        Altimeter = 6,
+        Magnetometer = 7,
+        PressureSensor = 8,
+        TemperatureSensor = 9,
+        HumiditySensor = 10,
+        RadiationSensor = 11,
+        Mission1 = 12,
+        Mission2 = 13,
+        Mission3 = 14,
+        Mission4 = 15,
     }
 }
